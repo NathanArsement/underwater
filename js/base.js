b75927289e812f3a64cd4e34f28ffd23b403cad0
Nathan Arsement <nathanarsement@gmail.com>
2023-05-05 12:40:41 +0530

    Add files via upload

--- js/base.js
+++ js/base.js
@@ -72,24 +72,81 @@ AFRAME.registerComponent('coin', {
 
 AFRAME.registerComponent('gameplay', {
   schema: {
-    elementID: { type: "string", default: "#target" },
-    score:{type:"number",default:0}
+    elementID: { type: "string", default: "#coin" }
   },
   isColided: function (elementID) {
     const element = document.querySelector(elementID)
     element.addEventListener("collide", e => {
       if (elementID.includes("#coin")) {
-        console.log(elementID + "colision");
-        this.data.score+=100;
+        this.updateScore()
+        this.updateTarget()
+        element.setAttribute("visible", false)
       }
-      else if (elementID.includes("#fish")) {
-        console.log("Fish Found")
-        console.log("Game Over")
+      else {
+        this.gameOver()
+
       }
     })
   },
+  init: function () {
+    var duration = 120
+    var timerEL = document.querySelector("#timer")
+    this.startTimer(duration, timerEL)
+  },
+
   update: function () {
     this.isColided(this.data.elementID)
   },
+  startTimer: function (duration, timerEL) {
+    var minutes;
+    var seconds
+    setInterval(() => {
+      if (duration > 0) {
+        minutes = parseInt(duration / 60)
+        seconds = parseInt(duration % 60)
+        if (minutes < 10) {
+          minutes = "0" + minutes
+        } if (seconds < 10) {
+          seconds = "0" + seconds
+        }
+        timerEL.setAttribute("text", {
+          value: minutes + ":" + seconds
+        })
+        duration -= 1
+      } else {
+        this.gameOver()
+      }
+    }, 1000)
+  },
+  gameOver: function () {
+    var planeEL = document.querySelector("#diver_model");
+    var element = document.querySelector("#game_over_text");
+    element.setAttribute("visible", true);
+    planeEL.setAttribute("dynamic-body", { mass: 5 });
+    planeEL.removeAttribute("foo")
+  },
+  updateTarget: function () {
+    var element = document.querySelector("#targets")
+    var count = element.getAttribute("text").value
+    console.log(count)
+    var currentTargets = parseInt(count)
+    currentTargets -= 1
+    element.setAttribute("text", { value: currentTargets })
+  },
+  updateScore: function () {
+    var element = document.querySelector("#scoreDisplay")
+    var count = element.getAttribute("text").value
+    console.log(count)
+    var currentScore = parseInt(count)
+    currentScore += 50
+    element.setAttribute("text", { value: currentScore })
+  },
+  remove: function () {
+    // Do something the component or its entity is detached.
+  },
 
+  tick: function (time, timeDelta) {
+    // Do something on every scene tick or frame.
+  }
 });
+
